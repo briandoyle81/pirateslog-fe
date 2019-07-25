fb60fe47c7b26b75e094d28fb9d6524619a8087b
briandoyle81 <briandoyle81@gmail.com>
2019-07-25 13:39:00 -0600

    removed console logs

--- src/components/EnterLogForm.js
+++ src/components/EnterLogForm.js
@@ -124,7 +124,6 @@ function EnterLog(props) {
       logValues.treasure = props.data.logEntryToEdit.treasure;
       logValues.tears = props.data.logEntryToEdit.tears;
       logValues.island = suggestions.filter(island => island.label === props.data.logEntryToEdit.island)[0];
-      console.log(logValues.island);
       logValues.crew = crewSuggestions.filter(profile => props.data.logEntryToEdit.crew.includes(profile.label));
       logValues.myShip = props.data.logEntryToEdit.myShip;
       logValues.loss = props.data.logEntryToEdit.loss;
@@ -132,7 +131,6 @@ function EnterLog(props) {
       setSelectedDate(props.data.logEntryToEdit.encounterTime)
       setlogEntryToEditID(props.data.logEntryToEdit.id)
     }
-    console.log(values.island)
   }, [props.data.openForm, props.data.logEntryToEdit]) // To avoid extra calls, DO NOT put `suggestions` here 
 
   function handleSubmit() {
